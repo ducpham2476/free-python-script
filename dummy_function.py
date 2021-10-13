1b080b672443d2679fe77793602ae67df15c735d
ducpham2476 <terangmd113@gmail.com>
2021-10-13 21:33:19 +0700

    First commit, dummy function (9:30 p.m, 13/10/2021)

---
+++ dummy_function.py
@@ -0,0 +1,13 @@
+import os
+
+
+def dummy():
+
+    # Get some value
+    current_working_dir = os.getcwd()
+
+    # Print something
+    print("Dummy function")
+    print("Current working directory: ", current_working_dir)
+
+dummy()
